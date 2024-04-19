a61f6abe2093c341c1cf8a0cb5e2491be2f1d794
Kevin Poorman <kjp@codefriar.com>
2024-04-19 00:19:50 -0700

    build

--- lib-old/git-actions.d.ts
+++
@@ -1,7 +0,0 @@
-import { context } from "@actions/github";
-export type GithubPullRequest = typeof context.payload.pull_request | undefined;
-/**
- * @description Calculates the diff for all files within the pull request and
- * populates a map of filePath -> Set of changed line numbers
- */
-export declare function getDiffInPullRequest(baseRef: string, headRef: string, destination?: string): Promise<Map<string, Set<number>>>;
